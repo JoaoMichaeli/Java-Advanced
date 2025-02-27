7439402a527ecc4fe6f5ea56ab821d01b2df003a
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-02-27 11:06:41 -0300

    add

---
+++ fin-money-api/src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package br.com.fiap.fin_money_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.fin_money_api.model.Category;
+
+@RestController
+public class CategoryController {
+    
+    @GetMapping("/categories")
+    public Category index(){
+        return new Category(1L, "Educação", "book");
+    }
+
+    @PostMapping("/categories")
+    public void create(){
+        System.out.println("Cadastrando categoria");
+    }
+
+}
