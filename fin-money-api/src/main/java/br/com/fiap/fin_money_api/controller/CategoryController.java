25b4d5b28bab68de3ab0c96aa82831eef41f8154
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-03-07 12:07:22 -0300

    att

--- fin-money-api/src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ fin-money-api/src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -1,76 +1,76 @@
 package br.com.fiap.fin_money_api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import br.com.fiap.fin_money_api.model.Category;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestController
-@RequestMapping("/categories")
-public class CategoryController {
-    private Logger log = LoggerFactory.getLogger(getClass());
+import br.com.fiap.fin_money_api.model.Category;
+import br.com.fiap.fin_money_api.repository.CategoryRepository;
 
-    private List<Category> repository = new ArrayList<>();
+@RestController
+@RequestMapping("categories")
+public class CategoryController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     
+    @Autowired
+    private CategoryRepository repository;
+
     @GetMapping
-    public List<Category> index(){
-        return repository;
+    public List<Category> index() {
+        return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Category create(@RequestBody Category category){
+    public Category create(@RequestBody Category category) {
         log.info("Cadastrando categoria " + category.getName());
-        repository.add(category);
+        repository.save(category);
         return category;
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Category> get(@PathVariable Long id){
-
+    public ResponseEntity<Category> get(@PathVariable Long id) {
         log.info("Buscando categoria " + id);
-
         return ResponseEntity.ok(getCategory(id));
     }
 
-    //apagar
     @DeleteMapping("{id}")
-    public ResponseEntity<Object> destroy(@PathVariable Long id){
-
-        log.info("Apagando categoria: " + id);
-
-        repository.remove(getCategory(id));
+    public ResponseEntity<Category> delete(@PathVariable Long id) {
+        log.info("Deletando categoria " + id);
+        repository.delete(getCategory(id));
         return ResponseEntity.noContent().build();
     }
 
-    //editar
     @PutMapping("{id}")
-    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category){
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category) {
+        log.info("Atualizando categoria " + id + " com " + category);
 
-        log.info("Atualizando categoria: " + id + " " + category);
-
-        repository.remove(getCategory(id));
+        getCategory(id);
         category.setId(id);
-        repository.add(category);
+        repository.save(category);
         return ResponseEntity.ok(category);
     }
 
     private Category getCategory(Long id) {
-        return repository.stream()
-                .filter(c -> c.getId().equals(id))
-                .findFirst()
+        return repository.findById(id)
                 .orElseThrow(
-                        () -> new ResponseStatusException(
-                                HttpStatus.NOT_FOUND,
-                                "Categoria não encontrada"
-                        )
+                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada")
                 );
     }
+
 }
