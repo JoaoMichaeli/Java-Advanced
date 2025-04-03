4fe72cca37b4c53a76296abc052e78229860ca42
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-04-03 12:01:38 -0300

    finMoney att

--- ProjetoFinMoney/API/src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
+++ ProjetoFinMoney/API/src/main/java/br/com/fiap/fin_money_api/exception/ValidationHandler.java
@@ -1,22 +1,26 @@
 package br.com.fiap.fin_money_api.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestControllerAdvice
 public class ValidationHandler{
 
-    record ValidationError(String field, String message){}
+    record ValidationError(String field, String message){
+        public ValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 
     @ExceptionHandler(exception = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public List<ValidationError> handler(MethodArgumentNotValidException e){
         return e.getFieldErrors()
                 .stream()
-                .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
+                .map(ValidationError::new)
                 .toList();
     };
 }
