f9627c1a6fcbedbb1297ff8cd6f50951db41579e
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-04-11 10:50:27 -0300

    att transaction

--- FinMoney/API/src/main/java/br/com/fiap/fin_money_api/controller/TransactionController.java
+++ FinMoney/API/src/main/java/br/com/fiap/fin_money_api/controller/TransactionController.java
@@ -2,22 +2,36 @@ package br.com.fiap.fin_money_api.controller;
 
 import br.com.fiap.fin_money_api.model.Transaction;
 import br.com.fiap.fin_money_api.repository.TransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequestMapping("transactions")
+@Slf4j
 public class TransactionController {
 
     @Autowired
     private TransactionRepository repository;
 
     @GetMapping
-    public List<Transaction> index(){
-        return repository.findAll();
+    public Page<Transaction> index(
+            @PageableDefault(size = 10, sort = "date", direction = Sort.Direction.DESC) Pageable pageable,
+            @RequestParam String description
+    ){
+        log.info("Buscando transações com descrição {}", description);
+
+        if(description != null) return repository.findByDescription(description, pageable);
+
+        return repository.findAll(pageable);
     }
+
 
 }
--- FinMoney/API/src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
+++ FinMoney/API/src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
@@ -1,6 +1,13 @@
 package br.com.fiap.fin_money_api.repository;
 
 import br.com.fiap.fin_money_api.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    //SELECT * FROM transaction WHERE description =: description
+    //@Query("SELECT t FROM transaction as t WHERE t.description =: description") //JPQL
+    Page<Transaction> findByDescription(String description, Pageable pageable);
+}
