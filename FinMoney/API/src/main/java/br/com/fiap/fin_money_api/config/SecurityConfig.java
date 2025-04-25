eea1139a55d736b844c3697cb201c030fd6f76fd
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-04-25 11:58:53 -0300

    att securityConfig

---
+++ FinMoney/API/src/main/java/br/com/fiap/fin_money_api/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package br.com.fiap.fin_money_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(
+                auth -> auth
+                        .requestMatchers("/categories/**")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()                
+        )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+
+        var users = List.of(
+                User
+                        .withUsername("joao")
+                        .password("$2a$12$FH9cPpPvuW4td6wBcr//8uw55f45mBP72Oj6PelTwco/awxZIis6a")
+                        .roles("ADMIN")
+                        .build(),
+                User
+                        .withUsername("maria")
+                        .password("$2a$12$5FQ2rvkCnOLDGb1M0DjFj.jVcJBGKBK9lJbaFe/W2NPfFvd4Hz4sm")
+                        .roles("USER")
+                        .build()
+        );
+
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- FinMoney/API/src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
+++ FinMoney/API/src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
@@ -1,8 +1,6 @@
 package br.com.fiap.fin_money_api.repository;
 
 import br.com.fiap.fin_money_api.model.Transaction;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
