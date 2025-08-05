3be6171bc4e3e857f778fc4023627c021f98ad50
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-08-05 11:56:41 -0300

    epictask

---
+++ 2 Semestre/epictask/src/main/java/br/com/fiap/epictask/task/TaskService.java
@@ -0,0 +1,18 @@
+package br.com.fiap.epictask.task;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+}
