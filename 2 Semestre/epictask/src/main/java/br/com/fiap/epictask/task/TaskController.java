89cf9be6db795b71955770ee5f15d30f9eeed3c2
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-08-19 12:01:13 -0300

    i18n

--- 2 Semestre/epictask/src/main/java/br/com/fiap/epictask/task/TaskController.java
+++ 2 Semestre/epictask/src/main/java/br/com/fiap/epictask/task/TaskController.java
@@ -1,7 +1,12 @@
 package br.com.fiap.epictask.task;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +14,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/task")
+@RequiredArgsConstructor
 public class TaskController {
 
     private final TaskService taskService;
-
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    private final MessageSource messageSource;
 
     @GetMapping
     public String index(Model model){
@@ -30,7 +33,11 @@ public class TaskController {
     }
 
     @PostMapping("/form")
-    public String create(Task task, RedirectAttributes redirect ){ //session
+    public String create(@Valid Task task, BindingResult result, RedirectAttributes redirect ){
+
+        if(result.hasErrors()) return "form";
+
+        var message = messageSource.getMessage("task.create.success", null, LocaleContextHolder.getLocale());
         taskService.save(task);
         redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso!");
         return "redirect:/task"; //301
